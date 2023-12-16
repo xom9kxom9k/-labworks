37967882ab685fe20c7618a55373a284213ab455
xom9kxom9k <vfomicev586@gmail.com>
2023-12-16 12:40:04 +0400

    First

---
+++ lab28/SortString.h
@@ -0,0 +1,15 @@
+#pragma once
+int LoadWords(char* filename);
+void fillArrayStrings();
+
+void SelectionSortStrings();
+void BubbleSortStrings();
+//void MergeSortStrings();
+void ShellSortStrings();
+void QuickSortStrings();
+
+//int LinearSearchStrings(char* value);
+//int BinarySearchStrings(char* value);
+int isSortedStrings();
+
+extern int n;
